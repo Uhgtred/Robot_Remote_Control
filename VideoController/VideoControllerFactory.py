5e86ab01947358ec8615c1974653fe6a30a65548
Uhgtred <Markus_koesters@gmx.de>
2024-03-04 22:25:48 +0100

    Update VideoControllerFactory.py

--- VideoController/VideoControllerFactory.py
+++ VideoController/VideoControllerFactory.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 # @author: Markus Kösters
 
+from .VideoController import VideoController
+from .VideoControllerConfig import VideoControllerConfig
+
 
 class VideoControllerFactory:
 
     @staticmethod
     def produceVideoController() -> VideoController:
-        return VideoController
+        config = VideoControllerConfig()
+        return VideoController(config)

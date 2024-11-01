bb876cd1585e5216750d993777f4afcd90358975
Markus Kösters <Markus_koesters@gmx.de>
2024-11-01 13:21:28 +0100

    Update Tcp_Udp_sockets.py

--- BusTransactions/BusPlugins/EthernetBusPlugin/Tcp_Udp_sockets.py
+++ BusTransactions/BusPlugins/EthernetBusPlugin/Tcp_Udp_sockets.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # @author: Markus Kösters
+
 import atexit
-import json
 import socket
 import struct
 
@@ -27,15 +27,9 @@ class UdpSocket(BusPluginInterface):
         Method that reads the UDP socket.
         :return: Message read from the UDP socket.
         """
-        # --- Receiving header containing message-length --- #
-        headerLength = struct.calcsize('Q')
-        # running loop until the size of message-length (headerLength (8byte)) has been reached
-        msgLength = self.__receiver(headerLength)
-        # unpacking the message-length
-        msgLength = int(struct.unpack('Q', msgLength)[0])
-        # --- Receiving header containing message-data --- #
-        msgData = self.__receiver(msgLength)
-        return msgData
+        headerLength = struct.calcsize('Q') # Todo: This is not a real header for udp. look at this: https://abdesol.medium.com/udp-protocol-with-a-header-implementation-in-python-b3d8dae9a74b
+        header, data = self.__receiver(headerLength)
+        return data
 
     def writeBus(self, message: bytes) -> None:
         """
@@ -49,35 +43,29 @@ class UdpSocket(BusPluginInterface):
     def _setupSocket(self, host: bool, sock: socket, port: int) -> None:
         """
         Private Method for setting up UDP-socket.
-        :param sock: socket that will be setup and bound.
+        This method is being called on instancing this class.
+        There should be no reason to call it directly.
+        :param sock: Socket that will be setup and bound.
         """
         # dynamically providing socket-ports for requested sockets.
         if port in self.__openSocketPorts:
-            raise BaseException('Port already in use')
             # check if the busLibrary-object has already been instanced
+            raise BaseException('Port already in use')
         self.sock = sock.socket(sock.AF_INET, sock.SOCK_DGRAM)
-        # self.sock = sock.socket(sock.AF_INET, sock.SOCK_DGRAM)
         if host:
             self.sock.bind((self.__myIPAddress, port))
             self.__openSocketPorts.add(port)
 
-    def __receiver(self, msgLength: int) -> bytes:
+    def __receiver(self, headerLength: int | None) -> tuple[bytes, bytes]:
         """
         Method that reads from a socket either message-header or message-body.
         :param msgLength: Length of the message that will be read from the socket.
                             Length of the body is represented by the header, which has length(struct.calcsize('Q')).
         :return: Message in bytes format.
         """
-        data = b''
-        while len(data) < msgLength:
-            # Varying receive-length to only receive the bytes of this specific message but max. self.__maxMessageSize!
-            rcvSize = self.__maxMessageSize if (msgLength - len(data)) > self.__maxMessageSize else (msgLength - len(data))
-            # receiving dynamic size of packets until every byte has been received
-            packet = self.sock.recvfrom(rcvSize)
-            if not packet[0]:
-                break
-            data += packet[0]
-        return data
+        message, address = self.sock.recvfrom(self.__maxMessageSize)
+        header, data = message[:headerLength], message[headerLength:]
+        return header, data
 
     def close(self) -> None:
         """

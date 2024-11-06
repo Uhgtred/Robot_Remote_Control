38f1865d93253a16b502655cb4fc938adc87fd2e
Markus Kösters <Markus_koesters@gmx.de>
2024-11-06 16:02:19 +0100

    adding a topbar and updating agp

--- GUI/RobotFrontend/composeApp/src/commonMain/kotlin/org/frontend/robotfrontend/App.kt
+++ GUI/RobotFrontend/composeApp/src/commonMain/kotlin/org/frontend/robotfrontend/App.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,6 +21,9 @@ import robotfrontend.composeapp.generated.resources.compose_multiplatform
 @Preview
 fun App() {
     MaterialTheme {
+        TopAppBar(){
+
+        }
         var showContent by remember { mutableStateOf(false) }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             Button(onClick = { showContent = !showContent }) {

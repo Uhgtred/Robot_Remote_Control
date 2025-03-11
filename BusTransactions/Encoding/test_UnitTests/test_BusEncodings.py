9b46e52f6916ff7cc9b25b95b5de11937f0c3007
uhgtred <markus_koesters@gmx.de>
2025-03-11 20:32:42 +0100

    Refactor UDP transceiver methods and enhance logging.
    Introduced `produceUDP_TransceiverWithStub` for clarity and separated stub handling from `produceUDP_Transceiver`. Added optional verbose logging for `writeSingleMessage` in `Bus.py` to improve debugging. Updated tests accordingly to use the new stub method.

--- BusTransactions/Encoding/test_UnitTests/test_BusEncodings.py
+++ BusTransactions/Encoding/test_UnitTests/test_BusEncodings.py
@@ -20,9 +20,9 @@ class test_BusEncodings(unittest.TestCase):
         """
         tests = []
         for method in dir(self.encoding):
-            self.__logger.debug(f'The encoding that is going to be tested is: {method}')
             if not method.startswith('__'):
                 method = getattr(self.encoding, method)()
+                self.__logger.debug(f'The encoding that is going to be tested is: {method}')
                 message = b'Hello World'
                 self.__logger.debug(f'The message that is going to be decoded is: {message}')
                 message = method.decode(message)

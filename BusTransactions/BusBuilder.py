1170ab49941dc1c15628336e4b88cbb49d7053a0
Uhgtred <Markus_koesters@gmx.de>
2025-05-02 14:53:55 +0200

    Update BusBuilder.py

--- BusTransactions/BusBuilder.py
+++ BusTransactions/BusBuilder.py
@@ -1,4 +1,4 @@
-import typing
+from typing import Self
 
 from BusTransactions.BusInterface import BusInterface
 from BusTransactions.Compression.CompressionProtocol import CompressionProtocol
@@ -14,15 +14,15 @@ class BusBuilder:
     def __init__(self, bus: type(BusInterface)) -> None:
         self.bus: BusInterface = bus()
 
-    def setCompressor(self, compressor: CompressionProtocol) -> typing.Self:
+    def setCompressor(self, compressor: CompressionProtocol) -> Self:
         self.bus.setCompressor(compressor)
         return self
 
-    def setSerializer(self, serializer: SerializationProtocol) -> typing.Self:
+    def setSerializer(self, serializer: SerializationProtocol) -> Self:
         self.bus.setSerializer(serializer)
         return self
 
-    def setEncoder(self, encoder: EncodingProtocol) -> typing.Self:
+    def setEncoder(self, encoder: EncodingProtocol) -> Self:
         self.bus.setEncoder(encoder)
         return self
 

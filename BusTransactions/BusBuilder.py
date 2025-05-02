732956aabe61fde50b7529e29d5d15e8afe9ed7f
Uhgtred <Markus_koesters@gmx.de>
2025-05-02 15:07:28 +0200

    adaptin python-version

--- BusTransactions/BusBuilder.py
+++ BusTransactions/BusBuilder.py
@@ -1,28 +1,25 @@
-from typing import Self
+import typing
 
 from BusTransactions.BusInterface import BusInterface
 from BusTransactions.Compression.CompressionProtocol import CompressionProtocol
 from BusTransactions.Encoding import EncodingProtocol
 from BusTransactions.Serialization.SerializationProtocol import SerializationProtocol
 
-"""
-Todo: get implementation done.
-"""
 
 class BusBuilder:
 
     def __init__(self, bus: type(BusInterface)) -> None:
         self.bus: BusInterface = bus()
 
-    def setCompressor(self, compressor: CompressionProtocol) -> Self:
+    def setCompressor(self, compressor: CompressionProtocol) -> typing.Self:
         self.bus.setCompressor(compressor)
         return self
 
-    def setSerializer(self, serializer: SerializationProtocol) -> Self:
+    def setSerializer(self, serializer: SerializationProtocol) -> typing.Self:
         self.bus.setSerializer(serializer)
         return self
 
-    def setEncoder(self, encoder: EncodingProtocol) -> Self:
+    def setEncoder(self, encoder: EncodingProtocol) -> typing.Self:
         self.bus.setEncoder(encoder)
         return self
 
